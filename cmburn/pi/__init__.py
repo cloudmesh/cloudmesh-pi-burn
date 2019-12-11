16593660e427fcc6148b5914c46ffb683a8d842a
Gregor von Laszewski <laszewski@gmail.com>
2019-12-11 08:41:36 -0500

    check for python3

--- cmburn/pi/__init__.py
+++ cmburn/pi/__init__.py
@@ -1,8 +1,10 @@
 import os
 import sys
+from cloudmesh.common.console import Console
 
 if sys.version_info[0] < 3:
-    raise Exception("You must be using Python 3")
+    Console.error("You must be using Python 3")
+    sys.exit()
 
 try:
     columns, lines = os.get_terminal_size()
