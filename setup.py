5540f7335429419cd07319aa29a5b91280b2fc15
Gregor von Laszewski <laszewski@gmail.com>
2020-04-11 18:27:25 -0400

    switsch setup.py classifier to
            "Programming Language :: Python",
            "Programming Language :: Python :: 3",
            "Programming Language :: Python :: 3.7",
            "Programming Language :: Python :: 3.8",

--- setup.py
+++ setup.py
@@ -74,8 +74,9 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     install_requires=requiers,
     tests_require=[
