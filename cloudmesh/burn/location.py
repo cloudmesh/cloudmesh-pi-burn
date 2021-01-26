e468687440897378defc70f5fd75a598af8ab007
Anthony Orlowski <aorlowski3@gatech.edu>
2021-01-26 10:07:06 -0500

    Added Ubuntu mount points

--- cloudmesh/burn/location.py
+++ cloudmesh/burn/location.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 class Location:
 
@@ -9,7 +10,7 @@ class Location:
         :param os: the os that is part of the mount. Default: raspberry
         :type os: str
         :param host: the host on which we execute the command
-        :type host: possible values: raspeberry, darwin
+        :type host: possible values: raspeberry, darwin, ubuntu
         """
         self.os = os or "raspberry"
         self.host = host = "raspberry"
@@ -25,9 +26,15 @@ class Location:
         :return: the location
         :rtype: str
         """
-        if self.os == "raspberry" and host =="darwin":
+        if self.os == "raspberry" and self.host =="darwin":
             raise "not supported without paragon"
             # return "/volume/???"
+        elif self.host == 'ubuntu':
+            user = os.environ.get('USER')
+            if "raspberry" in self.os:
+                return Path(f"/media/{user}/rootfs")
+            if "ubuntu" in self.os:
+                return Path(f"/media/{user}/writable")
 
     @property
     def boot_volume(self):
@@ -43,3 +50,9 @@ class Location:
                 return  Path("/Volume/boot")
             elif "ubuntu" in self.os:
                 return  Path("/Volume/system-boot")
+        elif host == "ubuntu":
+            user = os.environ.get('USER')
+            if "raspberry" in self.os:
+                return Path(f"/media/{user}/boot")
+            elif "ubuntu" in self.os:
+                return Path(f"/media/{user}/system-boot")
