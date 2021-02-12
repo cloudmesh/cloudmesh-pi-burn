19885425918e87fb0f6bb7fd8e4d60c3bc3d5937
Gregor von Laszewski <laszewski@gmail.com>
2021-02-12 05:42:29 -0500

    code cleanup

--- cloudmesh/burn/util.py
+++ cloudmesh/burn/util.py
@@ -1,30 +1,8 @@
 import os
 import platform
 import sys
+
 from cloudmesh.common.util import readfile
-
-import requests
-
-# noinspection PyPep8
-if False:
-    ##############################################
-    #
-    # Ignore on pi:  DH KEY TOO SMALL
-    #
-    # see: https://stackoverflow.com/questions/38015537/python-requests-exceptions-sslerror-dh-key-too-small
-    #
-
-    requests.packages.urllib3.disable_warnings()
-    requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS = 'AES128-SHA'
-    try:
-        # noinspection PyPep8
-        requests.packages.urllib3.contrib.pyopenssl.DEFAULT_SSL_CIPHER_LIST = 'AES128-SHA'
-    except AttributeError:
-        # no pyopenssl support used / needed / available
-        pass
-
-    #
-    ##############################################
 
 
 def os_is_windows():
