dee205b3bc302a29d24fc0ae0b81b51081044b63
Gregor von Laszewski <laszewski@gmail.com>
2021-02-17 19:02:36 -0500

    adjust path for fix

--- cloudmesh/burn/burner.py
+++ cloudmesh/burn/burner.py
@@ -904,7 +904,8 @@ class Burner(object):
 
         card = SDCard()
         fix = "/boot/fix_permissions.py"
-        writefile(f"{card.boot_volume}{fix}", script)
+        fix_on_sdcard = f"{card.boot_volume}/fix_permissions.py"
+        writefile(fix_on_sdcard, script)
 
         rc_local = f"{card.root_volume}/etc/rc.local"
         content = readfile(rc_local)
