c04cac39e7da1e76fab4873057c6452442cafb87
Gregor von Laszewski <laszewski@gmail.com>
2021-02-17 22:53:28 -0500

    remove spaces

--- cloudmesh/burn/burner.py
+++ cloudmesh/burn/burner.py
@@ -902,7 +902,7 @@ class Burner(object):
             for name, uid, guid, permission in files:
                 if os.path.exists(name):
                     os.chown(name, uid, guid)
-                    os.chmod(name, permission)
+                    os.chmod(name, permission)                
         """)
 
         card = SDCard()
