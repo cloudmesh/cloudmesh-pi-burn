da84da04f59e976be06ed322603f1ed510ef01d3
rickotten <ottenrichie@gmail.com>
2021-03-12 13:53:32 -0500

    Populate write in userdata

--- cloudmesh/burn/ubuntu/userdata.py
+++ cloudmesh/burn/ubuntu/userdata.py
@@ -1,5 +1,9 @@
 import yaml
 
+from cloudmesh.burn.sdcard import SDCard
+from cloudmesh.common.Shell import Shell
+from cloudmesh.common.util import path_expand, writefile
+
 class Userdata:
     HEADER = "#cloud-config"
     def __init__(self):
@@ -7,5 +11,11 @@ class Userdata:
 
     def __str__(self):
         return Userdata.HEADER + '\n' + yaml.dump(self.content)
+
     def write(self, filename=None):
-        raise NotImplementedError
+        """
+        Writes a file to a location. Safe write for files on mounted partitions
+        """
+        tmp_location = path_expand('~/.cloudmesh/user-data.tmp')
+        writefile(tmp_location, str(self))
+        Shell.execute('mv', [tmp_location, filename])
