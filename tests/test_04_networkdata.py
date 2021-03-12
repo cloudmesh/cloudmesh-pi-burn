9214f1e23f34f15982cf4d27c315d2aa443c59e3
rickotten <ottenrichie@gmail.com>
2021-03-11 22:27:56 -0500

    added initial pytest for networkdata

---
+++ tests/test_04_networkdata.py
@@ -0,0 +1,38 @@
+###############################################################
+# pytest -v --capture=no tests/test_04_networkdata.py
+# pytest -v  tests/test_04_networkdata.py
+# pytest -v --capture=no tests/test_04_networkdata.py::Test_burn::test_info
+###############################################################
+
+import pytest
+import textwrap
+import yaml
+
+from cloudmesh.burn.ubuntu.networkdata import Networkdata
+
+@pytest.mark.incremental
+class Test_Networkdata:
+	def test_static_ip(self):
+		d = Networkdata()\
+				.with_ip(ip="10.1.1.10")\
+				.with_gateway(gateway="10.1.1.1")\
+				.with_nameservers(nameservers=['8.8.8.8', '8.8.4.4'])\
+				.with_defaults().build()
+		
+		correct = {'version': 2, 
+						'ethernets': {
+							'eth0': {
+								'addresses': ['10.1.1.10/24'],
+								'dhcp4': 'no',
+								'gateway4': '10.1.1.1',
+								'nameservers': {
+									'addresses': ['8.8.8.8', '8.8.4.4']},
+									'match': {
+										'driver': 'bcmgenet smsc95xx lan78xx'
+									},
+									'set-name': 'eth0'}
+						},
+						'wifis': {}
+					}
+
+		assert(d == yaml.dump(correct))
