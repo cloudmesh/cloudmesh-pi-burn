dcfa9d451025db08ffff08c1f983220c9e44eda9
rickotten <ottenrichie@gmail.com>
2021-03-13 20:17:50 -0500

    added pytests for method call ordering

--- tests/test_05_userdata.py
+++ tests/test_05_userdata.py
@@ -11,4 +11,14 @@ class Test_Userdata:
 		correct = Userdata.HEADER + '\n' + yaml.dump(correct)
 		assert(correct == str(d))
 
+	def test_method_order(self):
+		d = Userdata().with_default_user().with_hostname(hostname='pytest').with_locale().with_ssh_password_login()
+		e = Userdata().with_hostname(hostname='pytest').with_locale().with_ssh_password_login().with_default_user()
+		f = Userdata().with_locale().with_ssh_password_login().with_default_user().with_hostname(hostname='pytest')
+		g = Userdata().with_ssh_password_login().with_default_user().with_hostname(hostname='pytest').with_locale()
 
+		d, e, f, g = d.content, e.content, f.content, g.content
+
+		assert(d == e)
+		assert(e == f)
+		assert(f == g)
