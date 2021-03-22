38dd5ab267a3c5bf10cbc15bc3031dd48d6ba62a
rickotten <ottenrichie@gmail.com>
2021-03-21 22:19:31 -0400

    Changed Cmdline to provide lines to append to cmdline.txt

--- cloudmesh/burn/raspberryos/cmdline.py
+++ cloudmesh/burn/raspberryos/cmdline.py
@@ -3,19 +3,30 @@ import textwrap
 class Cmdline:
 
     def __init__(self):
+        # self.script = " ".join(textwrap.dedent("""
+        # console=serial0,115200
+        # console=tty1
+        # root=PARTUUID=904a3764-02
+        # rootfstype=ext4
+        # elevator=deadline
+        # fsck.repair=yes
+        # rootwait
+        # quiet
+        # init=/usr/lib/raspi-config/init_resize.sh
+        # splash
+        # plymouth.ignore-serial-consoles
+        # systemd.run=/boot/firstrun.sh
+        # systemd.run_success_action=reboot
+        # systemd.unit=kernel-command-line.target
+        # """).splitlines()).strip()
+
+        # Commented out above since we should just append
+        # the lines below to the existing cmdline.txt since
+        # root PARTUUID may vary
         self.script = " ".join(textwrap.dedent("""
-        console=serial0,115200
-        console=tty1
-        root=PARTUUID=904a3764-02
-        rootfstype=ext4
-        elevator=deadline
-        fsck.repair=yes
-        rootwait
-        quiet
-        init=/usr/lib/raspi-config/init_resize.sh
         splash
         plymouth.ignore-serial-consoles
-        systemd.run=/boot/firstrun.sh
+        systemd.run=/boot/firstrun.sh 
         systemd.run_success_action=reboot
         systemd.unit=kernel-command-line.target
         """).splitlines()).strip()
