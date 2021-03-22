dececf0e2f1c247e6cfd1a5e355b647c96ffb9a2
Gregor von Laszewski <laszewski@gmail.com>
2021-03-22 08:59:11 -0400

    find ssid

---
+++ cloudmesh/burn/wifi/ssid.py
@@ -0,0 +1,28 @@
+from cloudmesh.common.Shell import Shell
+from cloudmesh.burn.util import os_is_linux
+from cloudmesh.burn.util import os_is_mac
+from cloudmesh.burn.util import os_is_pi
+from cloudmesh.burn.util import os_is_windows
+
+def get():
+    ssid = ""
+    try:
+        if os_is_mac():
+            command = "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I"
+            r = Shell.run(command).replace("\t", "").splitlines()
+            ssid = Shell.cm_grep(r, " SSID:")[0].split(":")[1].strip()
+        elif os_is_linux():
+            command = "iwgetid -r"
+            ssid = Shell.run(command).strip()
+        elif os_is_pi():
+            command = "iwgetid -r"
+            ssid = Shell.run(command).strip()
+        elif os_is_windows():
+            command = "netsh wlan show interfaces"
+            r = Shell.run(command).replace("\t", "").splitlines()
+            ssid = Shell.cm_grep(r, " SSID ")[0].split(":")[1].strip()
+    except:
+        pass
+
+    return ssid
+
