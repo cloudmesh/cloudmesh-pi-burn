a4f569c4635432af148504513e645c1c9c4c5335
Gregor von Laszewski <laszewski@gmail.com>
2021-03-22 09:28:19 -0400

    removed duplicated method

--- cloudmesh/burn/util.py
+++ cloudmesh/burn/util.py
@@ -73,15 +73,6 @@ def os_is_mac():
     """
     return platform.system() == "Darwin"
 
-def os_is_windows():
-    """
-    Check if the os is Windows
-
-    :return: True is Windows OS
-    :rtype: bool
-    """
-    return platform.system() == "Windows"
-
 # noinspection PyBroadException
 def os_is_pi():
     """
