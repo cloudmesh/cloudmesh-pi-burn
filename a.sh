bef4157a56e3772b4cfc9a49d40d8fa8bd151831
Gregor von Laszewski <laszewski@gmail.com>
2021-06-05 17:06:19 -0700

    update the a.sh script

--- a.sh
+++ a.sh
@@ -1,14 +1,24 @@
 # dd bs=4M if=/c/Users/venkata/.cloudmesh/cmburn/images/2021-03-04-raspios-buster-armhf-lite.img of=/dev/sdb conv=fdatasync status=progress
-        #     command = f"sudo dd if={image_path} bs={blocksize} |" \
-        #               f' tqdm --bytes --total {size} --ncols 80 |' \
-        #               f" sudo dd of={device} bs={blocksize}"
-        # else:
-        #     # command = f"sudo dd if={image_path} of={device} bs={blocksize} status=progress conv=fsync"
-        #     command = f"sudo dd if={image_path} bs={blocksize} oflag=direct |" \
-        #               f' tqdm --bytes --total {size} --ncols 80 |' \
-        #               f" sudo dd of={device} bs={blocksize} iflag=fullblock " \
-        #               f"oflag=direct conv=fsync"
+#     command = f"sudo dd if={image_path} bs={blocksize} |" \
+#               f' tqdm --bytes --total {size} --ncols 80 |' \
+#               f" sudo dd of={device} bs={blocksize}"
+# else:
+#     # command = f"sudo dd if={image_path} of={device} bs={blocksize} status=progress conv=fsync"
+#     command = f"sudo dd if={image_path} bs={blocksize} oflag=direct |" \
+#               f' tqdm --bytes --total {size} --ncols 80 |' \
+#               f" sudo dd of={device} bs={blocksize} iflag=fullblock " \
+#               f"oflag=direct conv=fsync"
+
 DVC_IGNORE_ISATTY=true
-dd bs=4M if=/c/Users/venkata/.cloudmesh/cmburn/images/2021-03-04-raspios-buster-armhf-lite.img oflag=direct | \
-tqdm --bytes --total 68719441552 --ncols 80 | \
-dd bs=4M of=/dev/sdb conv=fdatasync oflag=direct iflag=fullblock 
+FILE=/c/Users/blue/.cloudmesh/cmburn/images/2021-05-07-raspios-buster-armhf-lite.img
+SIZE=`stat --print="%s" $FILE`
+DEV=/dev/sdc
+
+echo $SIZE
+echo $FILE
+
+dd bs=4M if=$FILE oflag=direct |
+   tqdm --desc="format" --bytes --total=$SIZE --ncols=80 | \
+   dd bs=4M of=$DEV conv=fsync oflag=direct iflag=fullblock
+
+#dd bs=4M if=$FILE oflag=direct  | dd of=$DEV bs=4M status=progress conv=fsync oflag=direct iflag=fullblock
