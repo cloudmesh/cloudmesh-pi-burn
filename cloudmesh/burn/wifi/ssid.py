533bb0b6182082a5a1294dcea0d1b31949215c5a
dkkolli <dhakshesh.kolli@gmail.com>
2022-05-14 09:30:42 -0400

    For Windows machines on wifi, give user list of ssids to choose from rather than choosing the first ssid in list of ssids saved to their user profile

--- cloudmesh/burn/wifi/ssid.py
+++ cloudmesh/burn/wifi/ssid.py
@@ -3,10 +3,13 @@ from cloudmesh.burn.util import os_is_linux
 from cloudmesh.burn.util import os_is_mac
 from cloudmesh.burn.util import os_is_pi
 from cloudmesh.burn.util import os_is_windows
+from cloudmesh.common.Printer import Printer
+
 
 
 def get_ssid():
     ssid = ""
+    r = ""
     try:
         if os_is_mac():
             command = "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I"
@@ -19,21 +22,16 @@ def get_ssid():
             command = "iwgetid -r"
             ssid = Shell.run(command).strip()
         elif os_is_windows():
+            r = ''
             try:
-                command = " Netsh wlan show profiles | fgrep Profile"
-                r = Shell.run(command).replace("\t", "").splitlines()
-                ssid = Shell.cm_grep(r, " SSID ")[0].split(":")[1].strip()
+                command = " netsh wlan show profiles"
+                r = Shell.run(command).splitlines()
+                r = Shell.cm_grep(r, "User Profile")
+                r = [line.split(":")[1].strip() for line in r]
+                print('Found ssids:',r)
+                ssid = input('Enter ssid from list:')
             except:
-                ssid = None
-            if ssid is None:
-                try:
-                    command = " netsh wlan show profiles"
-                    r = Shell.run(command).splitlines()
-                    ssid = Shell.cm_grep(r, "User Profile")[0].split(":")[1].strip()
-
-                except:
-                    pass
+                pass
     except:  # noqa
         pass
-
     return ssid
