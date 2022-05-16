39a0d38717240ccfbdfbabdd08cb2cfd4e27db45
dkkolli <dhakshesh.kolli@gmail.com>
2022-05-16 16:34:16 -0400

    handle wired connections

--- cloudmesh/burn/wifi/ssid.py
+++ cloudmesh/burn/wifi/ssid.py
@@ -4,6 +4,7 @@ from cloudmesh.burn.util import os_is_mac
 from cloudmesh.burn.util import os_is_pi
 from cloudmesh.burn.util import os_is_windows
 from cloudmesh.common.Printer import Printer
+import subprocess
 
 
 
@@ -25,16 +26,14 @@ def get_ssid():
             r = ''
             try:
                 command = " netsh wlan show profiles"
-                r = Shell.run(command).splitlines()
-                if "The Wireless AutoConfig Service (wlansvc) is not running." in r:
-                    return None
-                else:
-                    r = Shell.cm_grep(r, "User Profile")
-                    r = [line.split(":")[1].strip() for line in r]
-                    print('Found ssids:',r)
-                    ssid = input('Enter ssid from list:')
-            except:
-                pass
+                r = Shell.run(command)#.splitlines()
+                r = Shell.cm_grep(r, "User Profile")
+                r = [line.split(":")[1].strip() for line in r]
+                print('Found ssids:',r)
+                ssid = input('Enter ssid from list:')
+            except subprocess.CalledProcessError as e:
+                if "The Wireless AutoConfig Service (wlansvc) is not running" in str(e.output):
+                    print("Machine is not configured for wifi")
     except:  # noqa
         pass
     return ssid
