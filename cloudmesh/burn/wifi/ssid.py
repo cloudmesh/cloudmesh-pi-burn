e6a3ef1cc297812a694245b06a0ac273e617428c
J.P <stapmoshun@gmail.com>
2022-05-23 10:01:35 -0400

    fix ssid

--- cloudmesh/burn/wifi/ssid.py
+++ cloudmesh/burn/wifi/ssid.py
@@ -1,8 +1,8 @@
 from cloudmesh.common.Shell import Shell
-from cloudmesh.burn.util import os_is_linux
-from cloudmesh.burn.util import os_is_mac
-from cloudmesh.burn.util import os_is_pi
-from cloudmesh.burn.util import os_is_windows
+from cloudmesh.common.systeminfo import os_is_linux
+from cloudmesh.common.systeminfo import os_is_mac
+from cloudmesh.common.systeminfo import os_is_pi
+from cloudmesh.common.systeminfo import os_is_windows
 
 
 def get_ssid():
@@ -20,19 +20,10 @@ def get_ssid():
             ssid = Shell.run(command).strip()
         elif os_is_windows():
             try:
-                command = " Netsh wlan show profiles | fgrep Profile"
-                r = Shell.run(command).replace("\t", "").splitlines()
-                ssid = Shell.cm_grep(r, " SSID ")[0].split(":")[1].strip()
+                r = Shell.run('netsh wlan show interfaces').strip().splitlines()
+                ssid = Shell.cm_grep(r, ' SSID')[0].split(':')[1].strip()
             except:
                 ssid = None
-            if ssid is None:
-                try:
-                    command = " netsh wlan show profiles"
-                    r = Shell.run(command).splitlines()
-                    ssid = Shell.cm_grep(r, "User Profile")[0].split(":")[1].strip()
-
-                except:
-                    pass
     except:  # noqa
         pass
 
