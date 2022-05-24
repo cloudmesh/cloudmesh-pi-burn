9a3978644fb43ad161072a0f3cfae74dc3553f0c
J.P <stapmoshun@gmail.com>
2022-05-24 12:35:03 -0400

    make ssid on CLI more clear

--- cloudmesh/burn/command/burn.py
+++ cloudmesh/burn/command/burn.py
@@ -556,7 +556,8 @@ class BurnCommand(PluginCommand):
                     if not ssid:
                         ssid = get_ssid()
                         if ssid is None:
-                            Console.info('Wireless connection not detected. Skipping SSID')
+                            Console.info('Wireless connection not detected. The user can specify the SSID when running '
+                                         'this command with --ssid=SSID. Skipping SSID')
                         else:
                             Console.ok(f"Using SSID: {ssid}")
                     if not wifipasswd and ssid:
